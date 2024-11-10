a6a929ae9b415a0e790a75c4ea080bbb656c6df9
Cyberchopin <wangshiyue1128@gmail.com>
2024-11-10 12:28:45 -0800

    create repo

---
+++ a.js
@@ -0,0 +1,16 @@
+// community/CommunityPanel.js
+function CommunityPanel() {
+    return (
+        <div>
+            {/* ... existing community panel content ... */}
+            
+            {/* 添加浮窗内容 */}
+            <div className="floating-panel">
+                <h2>✨ Focus Flow - Your ADHD-friendly reading companion!</h2>
+                <p>Make reading a breeze with our attention-boosting plugin 🎯 Try it now! 👉 <a href="[link]">link</a></p>
+            </div>
+            
+            {/* ... existing community panel content ... */}
+        </div>
+    );
+}
